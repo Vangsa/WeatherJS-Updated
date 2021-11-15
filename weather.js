6da11db647a3acf4a50489379d25a54b2779b4fa
Vangsa <82134109+Vangsa@users.noreply.github.com>
2021-11-15 11:56:30 +1100

    Add files via upload

--- weather.js
+++ weather.js
@@ -1,16 +1,12 @@
 class Weather {
     constructor(city, state){
-        this.apiKey = '3a33f5d30b5f140cca2d9ef9e0062b11';
         this.city = city;
         this.state = state;
     }
 
-    // Fetch Weather from API. Using OpenWeatherMap instead of Brad's Weather Underground,
-    // as Weather Underground no longer give out free API keys for personal use.
-
     async getWeather(){
         
-        const response = await fetch(`api.openweathermap.org/data/2.5/weather?q=${this.city},${this.state}&appid=${this.apiKey}`);
+        const response = await fetch(`api.openweathermap.org/data/2.5/weather?q=${this.city},${this.state}&appid=$3a33f5d30b5f140cca2d9ef9e0062b11`);
         const responseData = await response.json();
 
         return responseData;
